05e64e0821e063fc21631fa5de1cde2640809367
dom_z <xzhoubingjie@gmail.com>
2026-05-25 13:38:05 +0800

    chore: clean up temporary and old files

--- tmp/find_89.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open("tmp/constellations.json", "r", encoding="utf-8") as f:
-    const_data = json.load(f)
-
-const_ids = sorted([feat["id"] for feat in const_data["features"]])
-print(const_ids)
